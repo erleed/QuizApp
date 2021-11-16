ac861723b34706a09486b29374d7a1b441004faf
edelorbe <leetek3000@gmail.com>
2021-11-15 18:48:09 -0600

    fixes implemented.  Need to overveiw

--- app/src/main/java/com/android/example/quizapp/MainActivity.java
+++ app/src/main/java/com/android/example/quizapp/MainActivity.java
@@ -24,32 +24,32 @@ public class MainActivity extends AppCompatActivity {
     /***
      * Name a major PC manufacturer?
      */
-    TextView q1MajorPCManuf;
+    TextView editTQ1_PCManuf;
 
     /***
      * What are examples of output devices?
      */
-    CheckBox[] question_2 = new CheckBox[4];
+    CheckBox[] checkbQ2_OutputDevices = new CheckBox[4];
 
     /***
      * Which is a popular PC processor manufacturer in the US.
      */
-    RadioButton question_3;
+    RadioButton radioBQ3_PCProcessors;
 
     /***
      * What is your favorite OS?
      */
-    EditText question_4;
+    EditText editTQ4_PopularOS;
 
     /***
      * Wired family of technologies to connect PC's in a network.
      */
-    EditText question_5;
+    EditText editTQ5_ConnectTech;
 
     /**
      * Answers for 1st questions.
      */
-    int[][] answer1 = {
+    int[][] pcManufacturer = {
             {100, 101, 108, 108}                //0
             , {72, 80}                          //1
             , {65, 99, 101, 114}                //2
@@ -61,21 +61,21 @@ public class MainActivity extends AppCompatActivity {
             , {0}                               //8
     };
 
-    String[] answers4 = {"chrome os", "windows", "linux", "macos", "mac os", "mac"};
+    String[] operatingSystems = {"chrome os", "windows", "linux", "macos", "mac os", "mac"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         result = findViewById(R.id.result);
-        q1MajorPCManuf = findViewById(R.id.question_1);
-        question_2[0] = findViewById(R.id.question_2_1);
-        question_2[1] = findViewById(R.id.question_2_2);
-        question_2[2] = findViewById(R.id.question_2_3);
-        question_2[3] = findViewById(R.id.question_2_4);
-        question_3 = findViewById(R.id.question_3_1);
-        question_4 = findViewById(R.id.question_4);
-        question_5 = findViewById(R.id.question_5);
+        editTQ1_PCManuf = findViewById(R.id.editTQ1_PCManuf);
+        checkbQ2_OutputDevices[0] = findViewById(R.id.checkbQ2_OutputDevices_1);
+        checkbQ2_OutputDevices[1] = findViewById(R.id.checkbQ2_OutputDevices_2);
+        checkbQ2_OutputDevices[2] = findViewById(R.id.checkbQ2_OutputDevices_3);
+        checkbQ2_OutputDevices[3] = findViewById(R.id.checkbQ2_OutputDevices_4);
+        radioBQ3_PCProcessors = findViewById(R.id.radioBQ3_PCProcessors_1);
+        editTQ4_PopularOS = findViewById(R.id.editTQ4_PopularOS);
+        editTQ5_ConnectTech = findViewById(R.id.editTQ5_ConnectTech);
     }
 
     /**
@@ -85,21 +85,21 @@ public class MainActivity extends AppCompatActivity {
      */
     public int eval_question_1() {
         int q1score = 0;
-        answer1[8][0] = 0;
-        String q1Answer = q1MajorPCManuf.getText().toString();
-        for (int i = 0; i < answer1.length - 1; i++) {
+        pcManufacturer[8][0] = 0;
+        String q1Answer = editTQ1_PCManuf.getText().toString();
+        for (int i = 0; i < pcManufacturer.length - 1; i++) {
             String str = "";
-            for (int j = 0; j < answer1[i].length; j++) {
-                str = str + Character.toString((char) answer1[i][j]);
+            for (int j = 0; j < pcManufacturer[i].length; j++) {
+                str = str + Character.toString((char) pcManufacturer[i][j]);
             }
             if (str.toLowerCase().matches(q1Answer.toLowerCase())) {
-                answer1[8][0] = 1;
+                pcManufacturer[8][0] = 1;
                 str = null;
                 break;
             }
         }
-        if (answer1[8][0] == 1) {
-            answer1[8][0] = 0;
+        if (pcManufacturer[8][0] == 1) {
+            pcManufacturer[8][0] = 0;
             q1score = 1;
         }
         return q1score;
@@ -112,11 +112,14 @@ public class MainActivity extends AppCompatActivity {
      */
     private int eval_question_2() {
         int q2Score = 0;
-        if (question_2[1].isChecked()) {
+        if (checkbQ2_OutputDevices[1].isChecked()) {
             q2Score = q2Score + 1;
         }
-        if (question_2[3].isChecked()) {
+        if (checkbQ2_OutputDevices[3].isChecked()) {
             q2Score = q2Score + 1;
+        }
+        if (checkbQ2_OutputDevices[0].isChecked() || checkbQ2_OutputDevices[2].isChecked()) {
+            q2Score = 0;
         }
         return q2Score;
     }
@@ -132,12 +135,12 @@ public class MainActivity extends AppCompatActivity {
         boolean checked = ((RadioButton) view).isChecked();
         //Check which radio button was clicked
         switch (view.getId()) {
-            case R.id.question_3_1:
+            case R.id.radioBQ3_PCProcessors_1:
                 if (checked)
                     q3Score = 1;
                 break;
-            case R.id.question_3_2:
-            case R.id.question_3_3:
+            case R.id.radioBQ3_PCProcessors_2:
+            case R.id.radioBQ3_PCProcessors_3:
                 if (checked)
                     q3Score = 0;
                 break;
@@ -152,9 +155,9 @@ public class MainActivity extends AppCompatActivity {
      */
     private int eval_question_4() {
         int q4score = 0;
-        String answer = question_4.getText().toString().toLowerCase(Locale.ROOT);
-        for (int i = 0; i < answers4.length; i++) {
-            if (answers4[i].matches(answer)) {
+        String answer = editTQ4_PopularOS.getText().toString().toLowerCase(Locale.ROOT);
+        for (int i = 0; i < operatingSystems.length; i++) {
+            if (operatingSystems[i].matches(answer)) {
                 q4score = 1;
             }
         }
@@ -168,7 +171,7 @@ public class MainActivity extends AppCompatActivity {
      */
     private int eval_question_5() {
         int q5score = 0;
-        String answer = question_5.getText().toString();
+        String answer = editTQ5_ConnectTech.getText().toString();
         if ("ethernet".matches(answer.toLowerCase()))
             q5score++;
         return q5score;
@@ -183,7 +186,7 @@ public class MainActivity extends AppCompatActivity {
         double score = 0;           //Score: variable for storing total correct answers.
         score = score + eval_question_1();
         score = score + eval_question_2();
-        score = score + eval_question_3(question_3);
+        score = score + eval_question_3(radioBQ3_PCProcessors);
         score = score + eval_question_4();
         score = score + eval_question_5();
         score = (score / 6.00) * 100.00;
